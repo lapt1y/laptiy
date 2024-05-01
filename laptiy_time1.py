c8d689fd6e73db664ff86964feff327e59b3c97b
lapt1y <129749784+lapt1y@users.noreply.github.com>
2024-05-01 17:26:20 +0300

    Add files via upload

---
+++ laptiy_time1.py
@@ -0,0 +1,75 @@
+import asyncio
+from datetime import datetime
+
+from telethon.errors.rpcerrorlist import RPCError
+from telethon.tl.types import Message
+
+from .. import loader, utils
+
+TARGET_DATE = datetime(2024, 5, 10)
+
+@loader.tds
+class Countdown(loader.Module):
+    """Countdown to May 10th"""
+
+    strings = {"name": "Countdown"}
+    strings_ru = {
+        "_cls_doc": "Обратный отсчёт до 10 мая",
+        "_cmd_doc_countdown": "Показывает время до 10 мая",
+    }
+
+    async def countdowncmd(self, message: Message):
+        """Shows time until May 10th"""
+        try:
+            m = await self.animate_countdown(
+                message,
+                interval=0.059,
+                inline=False,
+            )
+            await m.edit(utils.get_args_raw(message) or "❤️")
+        except RPCError as e:
+            if "Content of the message was not modified" in str(e):
+                pass  # Пропустить обработку этой ошибки
+            else:
+                raise e  # Вызвать ошибку, если это другая RPC ошибка
+
+    async def countdownicmd(self, message: Message):
+        """Shows time until May 10th [Inline]"""
+        try:
+            m = await self.animate_countdown(
+                message,
+                interval=0.059,
+                inline=True,
+            )
+            await m.edit(utils.get_args_raw(message) or "❤️")
+        except RPCError as e:
+            if "Content of the message was not modified" in str(e):
+                pass  # Пропустить обработку этой ошибки
+            else:
+                raise e  # Вызвать ошибку, если это другая RPC ошибка
+
+    async def animate_countdown(self, message: Message, interval: float, inline: bool):
+        while True:
+            delta = TARGET_DATE - datetime.now()
+            if delta.total_seconds() <= 0:
+                break
+            countdown_text = str(delta).split(".")[0]
+            m = await self.animate(
+                message,
+                [countdown_text],
+                interval=interval,
+                inline=inline,
+            )
+            await asyncio.sleep(interval)
+        return m
+
+    async def animate(self, message: Message, frames, interval: float, inline: bool):
+        for frame in frames:
+            m = await message.respond(frame, parse_mode=None, link_preview=False)
+            await asyncio.sleep(interval)
+            try:
+                await m.delete()
+            except:
+                pass
+        return m
+        
